283cd1550b691d65307bb17901e614605fe2a35a
memematvaz <memematvaz@gmail.com>
2020-05-26 13:43:00 +0200

    DOM

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-const budgetAsk = prompt('¿Cual es tu presupuesto semanal?');
+const budgetAsk = prompt('¿Cual es tu presupuesto mensual?');
+
+
+
 let quantityBudget;
+
+const form = document.getElementById('add-expense')
+const primary = document.querySelector('.primary')
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -14,6 +20,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+form.addEventListener('submit', submit)
+
 class Budget {
     constructor(budget) {
         this.budget = Number(budget);
@@ -25,6 +33,7 @@ class Budget {
     }
 }
 
+
 class Interface {
     addBudget(quantity) {
         const budgetSpan = document.querySelector('span#total');
@@ -33,4 +42,71 @@ class Interface {
         budgetSpan.innerHTML = `${quantity}`;
         restSpan.innerHTML = `${quantity}`;
     }
+
+    responseSubmit(message, type) {
+        const divMessage = document.createElement('div');
+        divMessage.classList.add('text-center', 'alert')
+        //class Bootstrap styles
+        if(type === 'error') {
+            divMessage.classList.add('alert-danger');
+        
+        } else {
+            divMessage.classList.add('alert-success');
+
+        }
+        divMessage.appendChild(document.createTextNode(message));
+        primary.insertBefore(divMessage, form)
+
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 2500 )
+    }
+
+    addExpenseList(nameExpense, quantityExpense){
+        const expensesList = document.querySelector('#expenses ul');
+
+        const li = document.createElement('li');
+        li.classList.add ('list-group-item','d-flex', 'justify-content-between', 'align-items-center');
+
+        li.innerText = `${nameExpense}`;
+
+        const spanMoney = document.createElement('span');
+        spanMoney.classList.add ('badge','badge-primary', 'badge-pill');
+
+        spanMoney.innerText = `${quantityExpense} €`;
+
+        li.appendChild(spanMoney);
+
+
+        expensesList.appendChild(li);
+
+    }
+
+}
+
+
+
+
+function submit(e) {
+    e.preventDefault();
+    const nameExpense = document.querySelector('#expense').value;
+    const quantityExpense = document.querySelector('#quantity').value;
+
+
+
+    const userInterface = new Interface();
+
+    if (nameExpense === '' || quantityExpense === '' ||  isNaN(quantityExpense)) {   userInterface.responseSubmit('Hubo un error', 'error')
+    } else {
+        userInterface.responseSubmit('Agregado' , 'correcto');
+     
+        nameExpense === ''; 
+        quantityExpense === '';
+
+        userInterface.addExpenseList(nameExpense, quantityExpense);
+    }
+
+
+
+
 }
